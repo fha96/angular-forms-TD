48269bb83470c2c8b5675f2b53c953cef1753ccb
fha96 <fahadalrimawe96@hotmail.com>
2023-07-08 16:17:06 +0300

    practice-template-driven approch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-forms';
+  @ViewChild('f') formElem?: NgForm;
+  submitted: boolean = false ;
+  user: {
+    username: string,
+    email: string,
+    secret: string,
+    gender: string
+  } = {
+    username:'',
+    email:'',
+    secret:'',
+    gender:''
+  }; 
+
+  defaultSelection: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+
+    // patchValue method won't override all controls( inputs ) like setValue method
+    // also we can solve it with two data binding approach of [(ngModel)]
+    this.formElem?.form.patchValue({
+      userdata:{
+        username: suggestedName
+      }
+    })
+  }
+
+  // first approach
+  // onSubmit(formRef: NgForm){
+  //   console.log(formRef);
+  // }
+
+  // second approach using ViewChild
+  onSubmit() {
+    this.submitted = true ;
+    console.log(this.formElem);
+    this.user.username = this.formElem?.form.value.userdata.username;
+    this.user.email = this.formElem?.form.value.userdata.email;
+    this.user.secret = this.formElem?.form.value.secret;
+    this.user.gender = this.formElem?.form.value.gender;
+    this.formElem?.reset();
+  }
 }
